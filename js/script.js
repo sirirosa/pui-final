815eb8f3b0752dcda29a44d735a81b2e15279872
sirirosa <77872353+sirirosa@users.noreply.github.com>
2022-12-09 21:54:48 -0500

    bounce animation

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 resetButton();
 
+// hover popup
 let labelBox = document.querySelector('.text-hover');
 const onMouseMove = (e) => {
     labelBox.style.left = e.pageX + 2 + 'px';
     labelBox.style.top = e.pageY + 2 + 'px';
 }
+
+// makes hover appear when a user mouses over it 
 document.addEventListener('mousemove', onMouseMove);
 
+// refreshes page with data from json file
 function resetButton() {
     document.querySelector("#emotionsTextData").innerHTML = "";
 
+    // limits to 20 texts and randomizes texts on refresh
     for (let i=0; i<20; i++) {
         let randomIndex = Math.floor(Math.random()*(textData.length+1));
         let i = textData[randomIndex];
         let node = document.createElement("p");
         node.innerText = i.Text;
+
+        // adds label with correct emotion on hover
 
         if (i.Emotion == "sadness") {
             node.classList.add("sadness-text");
@@ -25,6 +32,7 @@ function resetButton() {
             document.querySelector(".semantic-label").innerText = "Sadness 😓";
             }
         }
+
         else if (i.Emotion == "anger") {
             node.classList.add("anger-text");
             node.onmouseout = function () { 
@@ -34,6 +42,7 @@ function resetButton() {
                 document.querySelector(".semantic-label").innerText = "Anger 😠";
                 }
         }
+
         else if (i.Emotion == "fear") {
             node.classList.add("fear-text");
             node.onmouseout = function () { 
@@ -43,6 +52,7 @@ function resetButton() {
                 document.querySelector(".semantic-label").innerText = "Fear 😨";
                 }
         }
+
         else if (i.Emotion == "surprise") {
             node.classList.add("surprise-text");
             node.onmouseout = function () { 
@@ -52,6 +62,7 @@ function resetButton() {
                 document.querySelector(".semantic-label").innerText = "Surprise 😮";
                 }
         }
+
         else if (i.Emotion == "love") {
             node.classList.add("love-text");
             node.onmouseout = function () { 
@@ -61,6 +72,7 @@ function resetButton() {
                 document.querySelector(".semantic-label").innerText = "Love 🥰";
                 }
         }
+
         else if (i.Emotion == "happy") {
             node.classList.add("happy-text");
             node.onmouseout = function () { 
@@ -76,6 +88,8 @@ function resetButton() {
 }
 
 
+// filters texts by emotion (listed in json key)using buttons in top navbar 
+
 function sadnessButton() {
     document.querySelector("#emotionsTextData").innerHTML = "";
     let count = 0;
@@ -88,7 +102,6 @@ function sadnessButton() {
         }
     }
 }
-
 
 function angerButton() {
     document.querySelector("#emotionsTextData").innerHTML = "";
